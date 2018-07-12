927b5310ca6d2f94eeea46a7df44e22fa0d04978
Gabriel Mutua <mutua@ndunda.com>
2018-07-12 17:36:14 +0300

    Added few indicator tiles

--- sampledashboard.R
+++ sampledashboard.R
@@ -18,12 +18,17 @@ ui <- dashboardPage(
       # First tab content
       tabItem(tabName = "dashboard",
               fluidRow(
-                box(title = "Population Per year", status = "primary",solidHeader = TRUE, plotOutput("plot1", height = 250)),
+                  # A static infoBox
+                  infoBox("Number of Countries", 221, icon = icon("globe", lib = "glyphicon")),
+                  # Dynamic infoBoxes
+                  infoBoxOutput("PopulationBox"),
+                  infoBoxOutput("TBBox")
+              ),
+              
+              fluidRow(
+                box(title = "Population Per Year", status = "primary",solidHeader = TRUE, plotOutput("plot1", height = 250)),
+                box(title = "TB Occurance Per Year", status = "primary",solidHeader = TRUE, plotOutput("plot2", height = 250))
                 
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
               )
       ),
       
@@ -39,9 +44,28 @@ server <- function(input, output) {
   #select data before intiating selector.
   country_data = subset(tb_data,country == "Kenya")
   country_data  = country_data[,c("year","e_pop_num","e_inc_num")]
-
+  
+  output$PopulationBox <- renderInfoBox({
+    infoBox(
+      "Population Growth", paste0(round((((country_data[17,2] - country_data[1,2])/(country_data[1,2]))*100), 0), "%"), icon = icon("user", lib = "glyphicon"),
+      color = "purple"
+    )
+  })
+  output$TBBox <- renderInfoBox({
+    infoBox(
+      "Growth in TB Occurance", paste0(round((((country_data[17,3] - country_data[1,3])/(country_data[1,3]))*100), 0), "%"), icon = icon("arrow-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  
+  
   output$plot1 <- renderPlot({
     ggplot(data = country_data, aes(x=year, y=e_pop_num)) +
+      geom_line()
+  })
+  
+  output$plot2 <- renderPlot({
+    ggplot(data = country_data, aes(x=year, y=e_inc_num)) +
       geom_line()
   })
 }
